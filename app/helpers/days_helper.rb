ea7c120ec9530a9c6d95fc9611f7e4115c2aeda2
Joanna Rajewska <joanna.rajewska@netguru.pl>
2013-04-01 18:48:56 +0200

    fix calendar days view; create right links

--- app/helpers/days_helper.rb
+++ app/helpers/days_helper.rb
@@ -1,2 +1,26 @@
 module DaysHelper
+
+  def day_link day
+    return new_day unless day.present?
+    day.created_at.to_date == Date.today ? edit(day) : show(day)
+  end
+
+  def day_field day, date
+    date > Date.today || (date < Date.today && day.nil?) ? date.day : day_link(day)
+  end
+
+  private
+
+  def show day
+    link_to l(day.created_at.to_date, format: :day), day
+  end
+
+  def edit day
+    link_to l(day.created_at.to_date, format: :day), edit_day_path(day)
+  end
+
+  def new_day
+    link_to l(Date.today, format: :day), new_day_path
+  end
+
 end
