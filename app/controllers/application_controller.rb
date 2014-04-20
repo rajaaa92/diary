ab45fa7b6e3de781af6b32c05d94e8dec80b3cb6
Joanna Rajewska <joanna.rajewska@netguru.pl>
2014-04-20 19:11:49 +0100

    lock app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  lock
+
+  def lock_app
+    session.delete :lock_opened
+    redirect_to lock_login_path
+  end
+
 end
